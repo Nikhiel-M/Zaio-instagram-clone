3d765371dac53ee2d0416d6b227391499446326e
Nikhiel-M <164867669+Nikhiel-M@users.noreply.github.com>
2025-06-30 11:29:59 +0200

    options will now display Edit and Delete if you are the owner of the post

--- app.js
+++ app.js
@@ -1,147 +1,176 @@
 class App {
-    constructor() {
-        this.storage = firebase.storage();
-        this.firestore = firebase.firestore();
-        this.$fileInput = document.querySelector("#upload-file");
-        this.$caption = document.querySelector("#create-caption");
-        this.$uploadFileButton = document.querySelector("#upload-button");
-        this.$app = document.querySelector("#app");
-        this.$firebaseAuthContainer = document.querySelector("#firebaseui-auth-container");
-        this.$authUser = document.querySelector(".auth-user");
-        this.$uploadButton = document.querySelector(".upload-button");
-        this.$uploadPage = document.querySelector("#upload-page");
-        this.$uploadPage.style.display = "none";
-        this.$discardButton = document.querySelector(".discard-btn");
-        this.$postContainer = document.querySelector(".posts");
-        this.$options = document.querySelector('.options');
-        this.$optionsModal = document.querySelector('#options-modal');
-        this.$cancel = document.querySelector('.cancel')
-        this.$optionsModal.style.display = "none";
-        this.ui = new firebaseui.auth.AuthUI(firebase.auth());
-        this.handleAuth();
-        this.addEventListeners();
+  constructor() {
+    this.storage = firebase.storage();
+    this.firestore = firebase.firestore();
+    this.$fileInput = document.querySelector("#upload-file");
+    this.$caption = document.querySelector("#create-caption");
+    this.$uploadFileButton = document.querySelector("#upload-button");
+    this.$app = document.querySelector("#app");
+    this.$firebaseAuthContainer = document.querySelector(
+      "#firebaseui-auth-container"
+    );
+    this.$authUser = document.querySelector(".auth-user");
+    this.$uploadButton = document.querySelector(".upload-button");
+    this.$uploadPage = document.querySelector("#upload-page");
+    this.$uploadPage.style.display = "none";
+    this.$discardButton = document.querySelector(".discard-btn");
+    this.$postContainer = document.querySelector(".posts");
+    this.$options = document.querySelector(".options");
+    this.$optionsModal = document.querySelector("#options-modal");
+    this.$cancel = document.querySelector(".cancel");
+    this.$optionsModal.style.display = "none";
+    this.$modalContent = document.querySelector("modal-content");
+    this.ui = new firebaseui.auth.AuthUI(firebase.auth());
+    this.handleAuth();
+    this.addEventListeners();
 
-        this.$authUser.addEventListener("click", (event) => {
-            event.preventDefault();
-            this.handleLogout();
-        });
+    this.$authUser.addEventListener("click", (event) => {
+      event.preventDefault();
+      this.handleLogout();
+    });
 
-        this.$uploadButton.addEventListener("click", (event) => {
-            this.handleUpload();
-        });
+    this.$uploadButton.addEventListener("click", (event) => {
+      this.handleUpload();
+    });
 
-        this.$uploadFileButton.addEventListener("click", (event) => {
-            event.preventDefault();
-            this.saveToStorage();
-        });
+    this.$uploadFileButton.addEventListener("click", (event) => {
+      event.preventDefault();
+      this.saveToStorage();
+    });
 
-        this.$postContainer.addEventListener("click", (event) => {
-          if(event.target.closest('.options')) {
-            event.preventDefault();
-            this.$optionsModal.style.display = "block";
-          }
+this.$postContainer.addEventListener("click", (event) => {
+  const optionsBtn = event.target.closest(".options");
+  if (optionsBtn) {
+    event.preventDefault();
+    const postDiv = optionsBtn.closest(".post");
+    const postUserId = postDiv.getAttribute("data-userid");
+    const isOwner = this.checkOwner({ userId: postUserId });
+
+    const listItems = this.$optionsModal.querySelectorAll("li");
+    if (isOwner) {
+      listItems[0].textContent = "Edit";
+      listItems[1].textContent = "Delete";
+    } else {
+      listItems[0].textContent = "Report";
+      listItems[1].textContent = "Unfollow";
+    }
+    this.$optionsModal.style.display = "block";
+  }
+});
+
+    this.$cancel.addEventListener("click", (event) => {
+      event.preventDefault();
+      this.$optionsModal.style.display = "none";
+    });
+  }
+
+  saveToStorage() {
+    const file = this.$fileInput.files[0];
+    if (!file) return;
+
+    const captionValue = this.$caption.value;
+    const user = firebase.auth().currentUser;
+    if (!user) return;
+    const displayName = user.displayName;
+    const storageRef = this.storage.ref("images/" + file.name);
+    const uploadTask = storageRef.put(file);
+    uploadTask.on("state_changed", null, null, () => {
+      storageRef
+        .getDownloadURL()
+        .then((url) => {
+          const imageData = {
+            userId: user.uid,
+            displayName: displayName,
+            photoURL: user.photoURL,
+            caption: captionValue,
+            url: url,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+          };
+          return this.firestore.collection("images").add(imageData);
         })
-
-        this.$cancel.addEventListener("click", (event) => {
-            event.preventDefault();
-            this.$optionsModal.style.display = "none";
-        })
-
-    }
-
-    saveToStorage() {
-        const file = this.$fileInput.files[0];
-        if (!file) return;
-
-        const captionValue = this.$caption.value;
-        const user = firebase.auth().currentUser;
-        if (!user) return;
-        const displayName = user.displayName;
-        const storageRef = this.storage.ref('images/' + file.name);
-        const uploadTask = storageRef.put(file);
-        uploadTask.on('state_changed', null, null, () => {
-            storageRef.getDownloadURL().then((url) => {
-                const imageData = {
-                    userId: user.uid,
-                    displayName: displayName,
-                    caption: captionValue,
-                    url: url,
-                    timestamp: firebase.firestore.FieldValue.serverTimestamp()
-                };
-                return this.firestore.collection('images').add(imageData);
-            }).then(() => {
-                this.$fileInput.value = '';
-                this.$caption.value = '';
-                this.handleDiscard();
-                this.displayPost(); 
-            });
+        .then(() => {
+          this.$fileInput.value = "";
+          this.$caption.value = "";
+          this.handleDiscard();
+          this.displayPost();
         });
-    }
+    });
+  }
 
-    handleUpload() {
-        this.$firebaseAuthContainer.style.display = "none";
-        this.$app.style.display = "none";
-        this.$uploadPage.style.display = "block";
-    }
+  handleUpload() {
+    this.$firebaseAuthContainer.style.display = "none";
+    this.$app.style.display = "none";
+    this.$uploadPage.style.display = "block";
+  }
 
-    handleDiscard() {
-        this.$firebaseAuthContainer.style.display = "none";
-        this.$app.style.display = "block";
-        this.$uploadPage.style.display = "none";
+  handleDiscard() {
+    this.$firebaseAuthContainer.style.display = "none";
+    this.$app.style.display = "block";
+    this.$uploadPage.style.display = "none";
+  }
+
+  handleAuth() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.$authUser.innerHTML = "Logout";
+        this.redirectToApp();
+        this.displayPost();
+      } else {
+        this.redirectToAuth();
       }
+    });
+  }
 
-    handleAuth() {
-        firebase.auth().onAuthStateChanged((user) => {
-            if (user) {
-                this.$authUser.innerHTML = "Logout";
-                this.redirectToApp();
-                this.displayPost();
-            } else {
-                this.redirectToAuth();
-            }
-        });
-    }
+  handleLogout() {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        this.redirectToAuth();
+      })
+      .catch((error) => {
+        console.log("ERROR OCCURRED", error);
+      });
+  }
 
-    handleLogout() {
-        firebase.auth().signOut().then(() => {
-            this.redirectToAuth();
-        }).catch((error) => {
-            console.log("ERROR OCCURRED", error);
-        });
-    }
+  redirectToApp() {
+    this.$firebaseAuthContainer.style.display = "none";
+    this.$app.style.display = "block";
+  }
 
-    redirectToApp() {
-        this.$firebaseAuthContainer.style.display = "none";
-        this.$app.style.display = "block";
-    }
+  redirectToAuth() {
+    this.$firebaseAuthContainer.style.display = "block";
+    this.$app.style.display = "none";
 
-    redirectToAuth() {
-        this.$firebaseAuthContainer.style.display = "block";
-        this.$app.style.display = "none";
+    this.ui.start("#firebaseui-auth-container", {
+      signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
+      // Other config options...
+    });
+  }
 
-        this.ui.start('#firebaseui-auth-container', {
-            signInOptions: [
-                firebase.auth.EmailAuthProvider.PROVIDER_ID
-            ],
-            // Other config options...
-        });
-    }
+  addEventListeners() {
+    this.$discardButton.addEventListener("click", (event) => {
+      this.handleDiscard();
+    });
+  }
 
-    addEventListeners() {
-        this.$discardButton.addEventListener("click", (event) => {
-          this.handleDiscard()
-        })
-      }
+checkOwner(post) {
+  const user = firebase.auth().currentUser;
+  if (!user) return false;
+  return user.uid === post.userId;
+}
 
-      
-
-      displayPost() {
-        this.$postContainer.innerHTML = "";
-        this.firestore.collection('images').orderBy('timestamp', 'desc').get().then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                const post = doc.data();
-                this.$postContainer.innerHTML += `
-                    <div class="post">
+  displayPost() {
+    this.$postContainer.innerHTML = "";
+    this.firestore
+      .collection("images")
+      .orderBy("timestamp", "desc")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          const post = doc.data();
+          this.$postContainer.innerHTML += `
+                    <div class="post" data-userid="${post.userId}">
                         <div class="header">
                             <div class="profile-area">
                                 <div class="post-pic">
@@ -165,13 +194,12 @@ class App {
                         </div>
                     </div>
                 `;
-            });
-        }).catch((error) => {
-            console.error("Error fetching posts:", error);
         });
-    }
-    
-    
+      })
+      .catch((error) => {
+        console.error("Error fetching posts:", error);
+      });
+  }
 }
 
 const InstagramApp = new App();
